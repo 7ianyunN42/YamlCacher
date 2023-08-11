15dd60a5a07f97fa36cb96760141c4fadac31a5b
7ianyunN42 <tianyun@neuro42.com>
2023-08-10 17:04:00 -0700

    Update lib_yaml_cacher.cpp

--- src/lib_yaml_cacher.cpp
+++ src/lib_yaml_cacher.cpp
@@ -2,29 +2,8 @@
 #include <methodobject.h>
 #include <object.h>
 #include "YamlCacher.h"
-long long factorial(int n) {
-    if (n <= 1)
-        return 1;
-    return n * factorial(n - 1);
-}
-
-static PyObject *py_template(PyObject *self, PyObject *args) {
-    int n;
-    if (!PyArg_ParseTuple(args, "i", &n))
-        return NULL;
-    return PyLong_FromLongLong(factorial(n));
-}
-
-static PyObject* py_print_int(PyObject* self, PyObject* args) {
-    int n;
-    if (!PyArg_ParseTuple(args, "i", &n))
-        Py_RETURN_NONE;
-    printf("%d\n", n);
-    Py_RETURN_NONE;
-}
 
 static PyObject* py_get_yaml(PyObject* self, PyObject* args) {
-
     // process inputs
     char* path = NULL;
     PyObject* py_list = NULL;
@@ -49,15 +28,13 @@ static PyObject* py_get_yaml(PyObject* self, PyObject* args) {
 
 
 static PyMethodDef module_methods[] = {
-    {"template", py_template, METH_VARARGS, "description of the template funciton"},
-    {"print_int", py_print_int, METH_VARARGS, "prints an integer"},
     {"get_yaml", py_get_yaml, METH_VARARGS, "Gets a yaml file"},
     {NULL, NULL, 0, NULL}
 };
 
 static struct PyModuleDef module_definition = {
     PyModuleDef_HEAD_INIT,
-    "template",
+    "yaml_cacher",
     NULL,
     -1,
     module_methods
