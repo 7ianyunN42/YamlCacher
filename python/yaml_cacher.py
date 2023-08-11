50248be9d76213aff3b985bbeff49f5a25f417cd
7ianyunN42 <tianyun@neuro42.com>
2023-08-10 17:43:00 -0700

    add documentation for python wrapper

--- python/yaml_cacher.py
+++ python/yaml_cacher.py
@@ -7,5 +7,17 @@ def get_yaml(absolute_path:str, keys:'list[str]'=None):
     ----------
     keys: list[str] or None
         A series of keys used to traverse the yaml file. If None, return the entire file.
+        
+    Returns
+    ----------
+    The value of the yaml file at the given keys. If keys is None, return the entire file.
+        
+    For example, if the yaml file is:
+    foo: [1, 2,3]
+    bar: {a: 1, b: 2}
+    
+    Calling get_yaml(absolute_path, ['bar']) will return {a: 1, b: 2}
+    Calling get_yaml(absolute_path, ['bar', 'a']) will return 1
+    Calling get_yaml(absolute_path) will return {'foo': [1, 2,3], 'bar': {'a': 1, 'b': 2}} 
     """
     return get_yaml_impl(absolute_path, keys)
