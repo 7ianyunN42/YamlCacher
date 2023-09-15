d69294aa51bf7db9ca8c61f630ccc41d47728014
7ianyunN42 <tianyun@neuro42.com>
2023-09-15 09:59:58 -0700

    add testing

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+INPUTS_PARENT_FOLDER = "test/unit_tests"
+PRINT_PASSED_TEST:bool = True
 
 import yaml
 from dist.yaml_cacher import get_yaml
@@ -14,20 +16,21 @@ def compare_yaml_cacher_to_python_lib(yml_path:str) -> bool:
      
     # recursively compare content in dictionary
     def compare_dicts(ref_dict, sol_dict):
+        ret:bool = True
         for key in ref_dict:
             if key not in sol_dict:
                 print(f"Key {key} not found in solution dictionary")
-                return False
+                ret = False
             if isinstance(ref_dict[key], dict):
                 if not isinstance(sol_dict[key], dict):
-                    print(f"Value of key {key}: expected: {ref_dict[key]}, got: {sol_dict[key]}")
-                    return False
+                    print(f"type error for {key}: expected: {type(ref_dict[key])}, got: {type(sol_dict[key])}")
+                    ret = False
                 if not compare_dicts(ref_dict[key], sol_dict[key]):
                     return False
             elif ref_dict[key] != sol_dict[key]:
-                print(f"Value of key {key}: expected: {ref_dict[key]}, got: {sol_dict[key]}")
-                return False
-        return True
+                print(f"value mismatch for {key}. expected: {ref_dict[key]} : {type(ref_dict[key])}, got: {sol_dict[key]} : {type(sol_dict[key])}")
+                ret = False
+        return ret
     
     return compare_dicts(ref_dict, sol_dict)
             
@@ -35,19 +38,20 @@ def compare_yaml_cacher_to_python_lib(yml_path:str) -> bool:
 def test_yaml_cacher():
     # iterate through all yml files in test folder
     import os
-    for root, dirs, files in os.walk("test/inputs"):
+    for root, dirs, files in os.walk(INPUTS_PARENT_FOLDER):
         for file in files:
             if file.endswith(".yml") or file.endswith(".yaml"):
                 yml_path = os.path.abspath(os.path.join(root, file))
                 if compare_yaml_cacher_to_python_lib(yml_path):
                     pass
-                    #print(f"{yml_path} - pass")
+                    print(f"{file} - pass") if PRINT_PASSED_TEST else None
                 else:
                     pass
-                   # print(f"{yml_path} - fail")
+                    print(f"{file} - fail")
 
 def main():
     test_yaml_cacher()
+    
 
 
 if __name__ == "__main__":
