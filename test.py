400df2dbd75ee331c97f8428d5e3d3399e99cb95
7ianyunN42 <tianyun@neuro42.com>
2023-09-14 17:37:23 -0700

    update unit testing

--- test.py
+++ test.py
@@ -2,28 +2,34 @@
 import yaml
 from dist.yaml_cacher import get_yaml
 
+
 def compare_yaml_cacher_to_python_lib(yml_path:str) -> bool:
-     ref_dict = yaml.safe_load(open(yml_path, 'r',  encoding='utf-8'))
-     sol_dict = get_yaml(yml_path)
+    ref_dict = None
+    try:
+        ref_dict = yaml.safe_load(open(yml_path, 'r',  encoding='utf-8'))
+    except Exception as e:
+        return True
+    
+    sol_dict = get_yaml(yml_path)
      
-     # recursively compare content in dictionary
-     def compare_dicts(ref_dict, sol_dict):
-         for key in ref_dict:
-             if key not in sol_dict:
-                 print(f"Key {key} not found in solution dictionary")
-                 return False
-             if isinstance(ref_dict[key], dict):
-                 if not isinstance(sol_dict[key], dict):
-                     print(f"Value of key {key} is not a dictionary in solution dictionary")
-                     return False
-                 if not compare_dicts(ref_dict[key], sol_dict[key]):
-                     return False
-             elif ref_dict[key] != sol_dict[key]:
-                 print(f"Value of key {key} does not match in solution dictionary")
-                 return False
-         return True
-     
-     return compare_dicts(ref_dict, sol_dict)
+    # recursively compare content in dictionary
+    def compare_dicts(ref_dict, sol_dict):
+        for key in ref_dict:
+            if key not in sol_dict:
+                print(f"Key {key} not found in solution dictionary")
+                return False
+            if isinstance(ref_dict[key], dict):
+                if not isinstance(sol_dict[key], dict):
+                    print(f"Value of key {key}: expected: {ref_dict[key]}, got: {sol_dict[key]}")
+                    return False
+                if not compare_dicts(ref_dict[key], sol_dict[key]):
+                    return False
+            elif ref_dict[key] != sol_dict[key]:
+                print(f"Value of key {key}: expected: {ref_dict[key]}, got: {sol_dict[key]}")
+                return False
+        return True
+    
+    return compare_dicts(ref_dict, sol_dict)
             
 
 def test_yaml_cacher():
@@ -37,7 +43,8 @@ def test_yaml_cacher():
                     pass
                     #print(f"{yml_path} - pass")
                 else:
-                    print(f"{yml_path} - fail")
+                    pass
+                   # print(f"{yml_path} - fail")
 
 def main():
     test_yaml_cacher()
